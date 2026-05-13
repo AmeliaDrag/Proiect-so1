8528a0d48d30733fe2ec7300b296f38f913010b8
AmeliaDrag <draganescuamelia@yahoo.com>
2026-05-13 11:06:30 +0300

    inceput 3

---
+++ city_hub.c
@@ -0,0 +1,1 @@
+//un pipe este un canal de comunicare intre 2 
---
+++ scores.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[]){
+  
+  return 0;
+  
+}
